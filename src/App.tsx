260a09c24a0087d066f0d3fbb7f70c1b5d330134
Simon Czerwinski <simon@czerwinski.se>
2023-08-28 17:51:52 +0200

    Updated header

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import MasterLayout from './layout/MasterLayout';
 const App: React.FC = () => {
 	return (
 		<MasterLayout>
-			<header className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+			<header className=" bg-black relative flex flex-wrap items-center justify-between p-8 lg:justify-center xl:px-0">
 				<h1 className="text-2xl font-bold text-white">
 					Welcome to my Robot Project
 				</h1>
 			</header>
-			<div className="bg-gray-200 py-4 flex flex-col items-center">
+			<div className="bg-gray-200 py-4 flex flex-col items-center m-10">
 				<h2 className="font-medium font-semibold mb-4">
 					Please choose a room layout
 				</h2>
