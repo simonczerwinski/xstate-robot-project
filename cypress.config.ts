375d9df299aecd1a7aa41c5670b0859db34add27
Simon Czerwinski <simon@czerwinski.se>
2023-09-05 19:12:25 +0200

    Added example test file to trigger cypress and check if it works

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+export default {
+	e2e: {
+		chromeWebSecurity: false,
+		viewportWidth: 1280,
+		viewportHeight: 800,
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+};
